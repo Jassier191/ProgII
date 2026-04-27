3004af0832ecaa471d52cd5d3e5f4c18d2013419
Jassier <jassierh19@gmail.com>
2026-04-27 13:25:27 -0500

    Hola mundo

---
+++ lab2/ParteA.py
@@ -0,0 +1,6 @@
+if True:
+ print("Primer IF con 1 espacio")
+
+if True:
+        print("Segundo IF con varios espacios")
+
---
+++ lab2/ParteB.py
@@ -0,0 +1,11 @@
+# Comillas dobles
+print("Hola mundo")
+
+# Números
+print(10)
+
+# Cálculos
+print(5 + 3)
+
+# Mezcla de texto y números
+print("Resultado:", 5 + 3)
