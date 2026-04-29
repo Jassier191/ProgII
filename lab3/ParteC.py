95f4fd9b65eed5b74081f4c17fe29a8f28d596ba
Jassier <jassierh19@gmail.com>
2026-04-29 03:06:14 -0500

    Hola mundo

--- lab3/ParteC.py
+++ lab3/ParteC.py
@@ -1,26 +1,22 @@
-# capitalize() - Convierte la primera letra en mayuscula
 texto = "hola mundo"
-print(texto.capitalize())  # Hola mundo
-# count() - Devuelve el numero de veces que un valor aparece en la cadena
+print(texto.capitalize())  
 frase = "banana"
-print(frase.count("a"))  # 3
-# find() - Busca un valor y devuelve la posicion donde se encontro
+print(frase.count("a"))  
 saludo = "Hello, World!"
-print(saludo.find("World"))  # 7
+print(saludo.find("World"))  
 
-# isalpha() - Devuelve True si todos los caracteres pertenecen al alfabeto
+
 texto = "Hola"
-print(texto.isalpha())  # True
+print(texto.isalpha()) 
 texto2 = "Hola123"
-print(texto2.isalpha())  # False
-# isdigit() - Devuelve True si todos los caracteres son digitos
-numero = "12345"
-print(numero.isdigit())  # True
-numero2 = "123abc"
-print(numero2.isdigit())  # False
-# isupper() - Devuelve True si todos los caracteres estan en mayusculas
-mayus = "HOLA MUNDO"
-print(mayus.isupper())  # True
-mezcla = "Hola Mundo"
-print(mezcla.isupper())  # False
+print(texto2.isalpha())  
 
+numero = "12345"
+print(numero.isdigit())  
+numero2 = "123abc"
+print(numero2.isdigit())  
+
+mayus = "HOLA MUNDO"
+print(mayus.isupper())  
+mezcla = "Hola Mundo"
+print(mezcla.isupper())  
