a0760e06330c276ea63febf97014162f49fa5ac4
Jassier <jassierh19@gmail.com>
2026-04-29 22:42:30 -0500

    Hola mundo

---
+++ lab4/ParteH.py
@@ -0,0 +1,7 @@
+provincias = ("Bocas del Toro","Coclé","Colón","Chiriquí","Darién","Herrera","Los Santos", "Panamá", "Veraguas")
+
+comarcas = ( "Guna Yala", "Ngäbe-Buglé", "Emberá-Wounaan")
+
+print("Provincias:", provincias)
+print("Comarcas:", comarcas)
+
