8ac47f487562970c26ada46a98711ab087672b62
jassier191 <jassierh19@gmail.com>
2026-05-04 20:13:47 -0500

    Hola mundo

---
+++ lab5/ParteB.py
@@ -0,0 +1,12 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1])
+ 
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+    print(x)
+
+tuple1 = ("a", "b", "c")
+tuple2 = (1, 2, 3)
+tuple3 = tuple1 + tuple2
+print(tuple3)
+ 
